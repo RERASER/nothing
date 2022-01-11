56d08a93c37f08050813f4be6a988c0a55605295
Kevin Trocolli <pitok236@gmail.com>
2022-01-10 20:31:01 -0500

    update dist start.bat for wacca

--- dist/mercury/start.bat
+++ dist/mercury/start.bat
@@ -2,8 +2,8 @@
 pushd %~dp0
 taskkill /f /im amdaemon.exe > nul 2>&1
 :LOOP
-inject -d -k mercuryhook.dll amdaemon.exe -c config.json config_region_jpn.json config_video_clone.json config_video_clone_flip.json config_video_dual.json config_video_dual_flip.json
-inject -d -k mercuryhook.dll ../WindowsNoEditor/Mercury.exe
+start inject -d -k mercuryhook.dll amdaemon.exe -f -c config.json config_region_jpn.json config_video_clone.json config_video_clone_flip.json config_video_dual.json config_video_dual_flip.json
+inject -d -k mercuryhook.dll ../WindowsNoEditor/Mercury/Binaries/Win64/Mercury-Win64-Shipping.exe
 taskkill /f /im amdaemon.exe > nul 2>&1
 echo.
 echo Game processes have terminated
