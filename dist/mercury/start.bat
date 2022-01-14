0fae1157344f2feddbc30d01cba1e8a1b5bba0d0
Kevin Trocolli <pitok236@gmail.com>
2022-01-14 16:07:26 -0500

    update wacca start.bat to more closely mirror official game.bat

--- dist/mercury/start.bat
+++ dist/mercury/start.bat
@@ -2,7 +2,7 @@
 pushd %~dp0
 taskkill /f /im amdaemon.exe > nul 2>&1
 :LOOP
-start inject -d -k mercuryhook.dll amdaemon.exe -f -c config.json config_region_jpn.json config_video_clone.json config_video_clone_flip.json config_video_dual.json config_video_dual_flip.json
+start inject -d -k mercuryhook.dll amdaemon.exe -f -c config.json config_lan_install_client.json config_lan_install_server.json config_video_clone.json config_video_dual.json config_video_clone_flip.json config_video_dual_flip.json config_region_exp.json config_region_chn.json config_region_jpn.json
 inject -d -k mercuryhook.dll ../WindowsNoEditor/Mercury/Binaries/Win64/Mercury-Win64-Shipping.exe
 taskkill /f /im amdaemon.exe > nul 2>&1
 echo.
